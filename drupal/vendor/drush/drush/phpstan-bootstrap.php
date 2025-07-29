c1640d8240655f6463b1cb92ccd61f24d7620afb
web-sharmasachin013 <93022768+web-sharmasachin013@users.noreply.github.com>
2025-07-29 07:23:04 +0000

    Remove deleted files

--- drupal/vendor/drush/drush/phpstan-bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Composer\Autoload\ClassLoader;
-
-// Deal with dynamic autoloader that we use for symfony cross version compat.
-$loader = new ClassLoader();
-$loader->addPsr4('Drush\\', 'src-symfony-compatibility/v6');
-$loader->register();
--- drupal/web/modules/contrib/commerce/modules/order/src/Exception/OrderLockedSaveException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Drupal\commerce_order\Exception;
-
-/**
- * Thrown when attempting to save an order that is locked for updating.
- */
-class OrderLockedSaveException extends \RuntimeException {}
