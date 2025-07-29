ce19613626dc55d1aa3c7f8a709671e57a798a6d
web-sharmasachin013 <93022768+web-sharmasachin013@users.noreply.github.com>
2025-07-29 01:00:45 +0000

    drupal config updated

---
+++ drupal/web/themes/drupalcustomcommerce/js/drupalcustomcommerce.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * drupalcustomcommerce behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.drupalcustomcommerce = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
